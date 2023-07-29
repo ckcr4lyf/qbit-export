d8f4340080fb340d00a17cabefdf6e5e7edb11c7
Raghu Saxena <poiasdpoiasd@live.com>
2023-07-29 18:29:17 +0800

    TODO

---
+++ bin/index.mjs
@@ -0,0 +1,9 @@
+/**
+ * TODO:
+ * 
+ * 1. List files in folder
+ * 2. Group the .torrent & .fastresume
+ * 3. (Sanity check) .torrent does not have announce / announce-list
+ * 4. Populate announce & announce-list from .fastresume)
+ * 5. Save as `export_${hash}.torrent`
+ */
